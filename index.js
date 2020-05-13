0343b9583ca00e80d1e6c13912e9f3d79db5a9b6
Adrian Parry <aparry@goplaceit.com>
2020-05-13 13:15:11 -0400

    Use env variable for PORT

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var cors = require("cors");
 var app = express();
 var router = require("./routes/index.js");
 
-var PORT = process.env.NODE_ENV === "development" ? 3030 : 16833;
+var PORT = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(router);
 
 app.listen(PORT, function () {
   console.log("Server started in port " + PORT);
-  console.log("database url = ", process.env.DATABASE_URL);
 });
