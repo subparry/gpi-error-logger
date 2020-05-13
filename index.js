a9cd90046ce3b820b16f5978a4717f145cd31558
Adrian Parry <aparry@goplaceit.com>
2020-05-13 12:50:37 -0400

    Changed server port for production

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var cors = require("cors");
 var app = express();
 var router = require("./routes/index.js");
 
+var PORT = process.env.NODE_ENV === "development" ? 3030 : 443;
+
 app.use(cors());
 app.use(router);
 
-app.listen(3030, function () {
-  console.log("Server started in port 3030");
+app.listen(PORT, function () {
+  console.log("Server started in port " + PORT);
 });
