deb3e9bb36fed76fa6a26b0189b680ed7e0952e7
Adrian Parry <aparry@goplaceit.com>
2020-05-13 13:06:41 -0400

    added engines to package.json and modified PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require("cors");
 var app = express();
 var router = require("./routes/index.js");
 
-var PORT = 3030;
+var PORT = process.env.NODE_ENV === "development" ? 3030 : 16833;
 
 app.use(cors());
 app.use(router);
