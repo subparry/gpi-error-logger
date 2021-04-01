03f37a5503916f8a1d4d3ea33e0befc2af80bab0
Adrian Parry <subparry@gmail.com>
2021-03-31 23:55:05 -0300

    added ssl to postgres connection

--- db/index.js
+++ db/index.js
@@ -1,24 +1,27 @@
-var pg = require("pg");
-var Pool = pg.Pool;
+var pg = require('pg')
+var Pool = pg.Pool
 
 var connectionConf =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? {
-        user: "gpilogger",
-        host: "localhost",
-        database: "gpiloggerdb",
-        password: "GPILogger",
+        user: 'gpilogger',
+        host: 'localhost',
+        database: 'gpiloggerdb',
+        password: 'GPILogger',
         port: 5432,
       }
-    : { connectionString: process.env.DATABASE_URL };
+    : {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
 
-var pool = new Pool(connectionConf);
+var pool = new Pool(connectionConf)
 
 module.exports = {
   query: function (sql) {
-    return pool.query(sql);
+    return pool.query(sql)
   },
   connect: function (cb) {
-    return pool.connect(cb);
+    return pool.connect(cb)
   },
-};
+}
