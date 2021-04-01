0dad92eeaa809e2023ebd3c7d9c287e728c9bc6f
Adrian Parry <subparry@gmail.com>
2021-04-01 00:04:42 -0300

    log error in post route

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,77 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express')
+var bodyParser = require('body-parser')
 
-var router = express.Router();
-var pool = require("../db");
-var jsonParser = bodyParser.json();
+var router = express.Router()
+var pool = require('../db')
+var jsonParser = bodyParser.json()
 
 function routeLogger(req, res, next) {
-  console.log("ROUTE LOGGER: url=" + req.url);
-  console.log("ROUTE LOGGER: method=" + req.method);
-  next();
+  console.log('ROUTE LOGGER: url=' + req.url)
+  console.log('ROUTE LOGGER: method=' + req.method)
+  next()
 }
 
-router.use(routeLogger);
+router.use(routeLogger)
 
-router.get("/", function (req, res) {
-  res.send("Root route. Visit /errors to get all current errors");
-});
+router.get('/', function (req, res) {
+  res.send('Root route. Visit /errors to get all current errors')
+})
 
-router.get("/errors", function (req, res) {
+router.get('/errors', function (req, res) {
   pool
-    .query("SELECT * FROM errors ORDER BY id DESC LIMIT 50")
+    .query('SELECT * FROM errors ORDER BY id DESC LIMIT 50')
     .then(function (data) {
-      res.send({ errors: data.rows });
-    });
-});
+      res.send({ errors: data.rows })
+    })
+})
 
-router.get("/errors/count", function (req, res) {
-  pool.query("SELECT COUNT(id) FROM errors;").then(function (data) {
-    res.send({ count: data.rows });
-  });
-});
+router.get('/errors/count', function (req, res) {
+  pool.query('SELECT COUNT(id) FROM errors;').then(function (data) {
+    res.send({ count: data.rows })
+  })
+})
 
-router.post("/errors", jsonParser, function (req, res) {
-  (async function () {
-    const client = await pool.connect();
+router.post('/errors', jsonParser, function (req, res) {
+  ;(async function () {
+    const client = await pool.connect()
 
     try {
-      await client.query("BEGIN");
+      await client.query('BEGIN')
       const result = await client.query(
-        "SELECT id, occurrence_count FROM errors WHERE type = $1 AND message = $2 AND url = $3",
+        'SELECT id, occurrence_count FROM errors WHERE type = $1 AND message = $2 AND url = $3',
         [req.body.type, req.body.message, req.body.url]
-      );
+      )
       if (result.rows.length) {
         await client.query(
-          "UPDATE errors SET occurrence_count = $1, last_occurrence_at = $2 WHERE id = $3",
+          'UPDATE errors SET occurrence_count = $1, last_occurrence_at = $2 WHERE id = $3',
           [result.rows[0].occurrence_count + 1, new Date(), result.rows[0].id]
-        );
+        )
       } else {
         const query = {
           text:
-            "INSERT INTO errors(type, message, first_occurrence_at, last_occurrence_at, url, browser, ip) VALUES($1, $2, $3, $4, $5, $6, $7)",
+            'INSERT INTO errors(type, message, first_occurrence_at, last_occurrence_at, url, browser, ip) VALUES($1, $2, $3, $4, $5, $6, $7)',
           values: [
-            req.body.type || "N/A",
-            req.body.message || "N/A",
+            req.body.type || 'N/A',
+            req.body.message || 'N/A',
             new Date(),
             new Date(),
-            req.body.url || "N/A",
-            req.body.browser || "N/A",
+            req.body.url || 'N/A',
+            req.body.browser || 'N/A',
             req.ip,
           ],
-        };
-        await client.query(query);
+        }
+        await client.query(query)
       }
-      await client.query("COMMIT");
-      res.status(201).send({ message: "ok" });
+      await client.query('COMMIT')
+      res.status(201).send({ message: 'ok' })
     } catch (e) {
-      await client.query("ROLLBACK");
-      res.status(500).send({ message: e.message });
+      await client.query('ROLLBACK')
+      console.log('ERROR: ', e.message)
+      res.status(500).send({ message: e.message })
     } finally {
-      client.release();
+      client.release()
     }
-  })().catch((e) => console.error(e.stack));
-});
+  })().catch((e) => console.error(e.stack))
+})
 
-module.exports = router;
+module.exports = router
